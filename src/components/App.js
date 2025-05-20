ffb8303be53cbedd4c67f8c5983f93961d78d8c6
Ajay70aa <ajay70aa@gmail.com>
2025-05-20 10:58:53 +0530

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,40 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
+  const projects = [
+    {
+      id: 1,
+      name: "Weather App",
+      description: "A web app to display weather information using API."
+    },
+    {
+      id: 2,
+      name: "Todo List",
+      description: "Manage daily tasks with a CRUD todo list app."
+    },
+    {
+      id: 3,
+      name: "E-Commerce Site",
+      description: "An online store with product listings and cart feature."
+    }
+  ];
+  const Project = ({name, description}) => {
+    return (
+      <div className="ns-wrapper">
+        <h1 data-ns-test="project-name">{name}</h1>
+        <h6 data-ns-test="project-description">{description}</h6>
+      </div>
+    )
+  }
   return (
-    <div id="main"></div>
+    <div id="main">
+      {projects.map((project) => {
+        return (
+          <Project key={project.id} name={project.name} description={project.description}/>
+        )
+      })}
+    </div>
   )
 }
-
 
 export default App;
